39dd9caab599f61d96346a1c6824750ac8b893ea
Frank Schiebel <frank@linuxmuster.net>
2017-01-01 19:58:58 +0100

    First working version for JS4

--- 03-nascopy.sh
+++ 03-nascopy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 BASE="/tmp"
-HOST="diskstation"
-FOLDER="documents"
-YEAR=`date '+%Y'`
+HOST="helga"
+SFTPUSER="paperbackup"
+FOLDER="~/documents"
+YEAR=$(date "+%Y")
+MONTH=$(date "+%m")
+
+# get wifi address
+WIFIIP=$(ip addr show  wlan0  | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+
+if [ $WIFIIP == "192.168.1.128" ]; then
+	HOST="192.168.1.2:2222"
+fi
+
+
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <jobid> <user> [<keyword>]"
@@ -20,7 +31,7 @@ if [ -z "$2" ]; then
 fi
 
 OUTPUT="$BASE/$1"
-REMOTE="sftp://$2@$HOST/home/$FOLDER/$YEAR/$3/$1.pdf"
+REMOTE="sftp://$SFTPUSER@$HOST/$FOLDER/$2/$3/$YEAR/$MONTH/$1.pdf"
 LOCAL="$OUTPUT/$1.pdf"
 
 if [ ! -f "$LOCAL" ]; then
@@ -29,6 +40,10 @@ if [ ! -f "$LOCAL" ]; then
 fi
 
 
+# For this to work, I had to create a dsa key für auth because 
+# curl kept refusing to use the rsa key.
+# The folder hat to be given as ~/documents, absolute paths didnt work 
+# I suspect because of the changeroot jail on the server
 echo copying to $REMOTE
 curl --ftp-create-dirs --insecure -T "$LOCAL" "$REMOTE"
 
